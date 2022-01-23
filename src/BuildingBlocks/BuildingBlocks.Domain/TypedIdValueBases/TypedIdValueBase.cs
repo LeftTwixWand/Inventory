51a5f0b1026f0391937196ba59c382edce4f6de0
Vladyslav <lefttwixwand@gmail.com>
2022-01-23 21:21:51 +0200

    feat: Add BuildingBlocks.Domain code base

---
+++ src/BuildingBlocks/BuildingBlocks.Domain/TypedIdValueBases/TypedIdValueBase.cs
@@ -0,0 +1,56 @@
+﻿namespace BuildingBlocks.Domain.TypedIdValueBases;
+
+public abstract class TypedIdValueBase : IEquatable<TypedIdValueBase>
+{
+    protected TypedIdValueBase(Guid value)
+    {
+        if (value == Guid.Empty)
+        {
+            throw new InvalidOperationException("Id value cannot be empty!");
+        }
+
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static bool operator ==(TypedIdValueBase obj1, TypedIdValueBase obj2)
+    {
+        if (object.Equals(obj1, null))
+        {
+            if (object.Equals(obj2, null))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        return obj1.Equals(obj2);
+    }
+
+    public static bool operator !=(TypedIdValueBase x, TypedIdValueBase y)
+    {
+        return !(x == y);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        return obj is TypedIdValueBase other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+
+    public bool Equals(TypedIdValueBase? other)
+    {
+        return this.Value == other?.Value;
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Domain/ValueObjects/IgnoreMemberAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingBlocks.Domain.ValueObjects;
+
+/// <summary>
+/// Marks, that current property or field will be ignored in ValueObject comparison.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public sealed class IgnoreMemberAttribute : Attribute
+{
+}
