5f2b2e4d5f6d8d6136122cdea25c3a44232399c1
Vladyslav <lefttwixwand@gmail.com>
2022-01-25 00:07:07 +0200

    feat: Add DomainEventHandler to Application Block

---
+++ src/BuildingBlocks/BuildingBlocks.Application/DomainEvents/IDomainEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.Domain.Events;
+using MediatR;
+
+namespace BuildingBlocks.Application.DomainEvents;
+
+public interface IDomainEventHandler<in TDomainEvent> :
+    INotificationHandler<TDomainEvent>
+    where TDomainEvent : IDomainEvent
+{
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Application/Validation/InvalidCommandException.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlocks.Application.Validation;
+
+public class InvalidCommandException : Exception
+{
+    public InvalidCommandException(string message, string details = "")
+        : base(message)
+    {
+        Details = details;
+    }
+
+    public string Details { get; }
+}
