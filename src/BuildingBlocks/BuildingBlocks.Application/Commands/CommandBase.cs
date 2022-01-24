8afd0c8421aede755d790a414c6eafd43c22e814
Vladyslav <lefttwixwand@gmail.com>
2022-01-24 23:50:57 +0200

    feat: Add Application layer Building Block

---
+++ src/BuildingBlocks/BuildingBlocks.Application/Commands/CommandBase.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.Application.Requests;
+
+namespace BuildingBlocks.Application.Commands;
+
+/// <summary>
+/// Abstract record, that encapsulate functionality of <see cref="ICommand"/> and adding request identity.
+/// </summary>
+public abstract record CommandBase : RequestBase, ICommand
+{
+}
+
+/// <summary>
+/// Abstract record, that encapsulate functionality of <see cref="ICommand{TResult}"/> and adding request identity.
+/// </summary>
+/// <typeparam name="TResult"><inheritdoc cref="RequestBase{TResult}" path="/typeparam"/></typeparam>
+public abstract record CommandBase<TResult> : RequestBase<TResult>, ICommand
+{
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Application/Requests/IRequestIdentity.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlocks.Application.Requests;
+
+/// <summary>
+/// Add unique identifyier for request.
+/// </summary>
+public interface IRequestIdentity
+{
+    /// <summary>
+    /// Unique identifyier of request.
+    /// </summary>
+    Guid Id { get; }
+}
