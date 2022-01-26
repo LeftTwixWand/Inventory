0932207f5593508d1825230f787c0ca4ebe3a56c
Vladyslav Horbachov <0681813464.dev@gmail.com>
2022-01-26 17:39:37 +0200

    feat(Presentation): Moved MainWindow to the presentation layer

--- src/Inventory.Presentation/Windows/MainWindows/MainWindow.xaml.cs
+++ src/Inventory.Presentation/Windows/MainWindows/MainWindow.xaml.cs
@@ -3,7 +3,7 @@
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Inventory;
+namespace Inventory.Presentation.Windows.MainWindows;
 
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
