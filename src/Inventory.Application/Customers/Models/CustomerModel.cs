15ffc9b541f8dd6f71072d0f1a91536ea3cb556a
Vladyslav <lefttwixwand@gmail.com>
2022-01-30 22:14:47 +0200

    feat(Presentation): Add customers pane

---
+++ src/Inventory.Application/Customers/Models/CustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Inventory.Application.Customers.Models;
+
+public class CustomerModel : ObservableObject
+{
+    public long CustomerID { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string EmailAddress { get; set; } = string.Empty;
+
+    public object ThumbnailSource { get; set; } = string.Empty;
+
+    public string FullName => $"{FirstName} {LastName}";
+}
