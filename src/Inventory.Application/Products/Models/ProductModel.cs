e95a972568d1411a6b47541b10c4a043c0c524e8
Vladyslav Horbachov <0681813464.dev@gmail.com>
2022-02-04 15:37:19 +0200

    feat(Presentation): Add Orders pane

---
+++ src/Inventory.Application/Products/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Inventory.Application.Products.Models;
+
+public class ProductModel : ObservableObject
+{
+    public long ProdcutID { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+}
