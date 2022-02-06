29eae3f562b3c2bc74dd15c53dad3e5826be09b5
Vladyslav <lefttwixwand@gmail.com>
2022-02-06 23:21:49 +0200

    feat(Application): Added GenericViewModel

---
+++ src/Inventory.Application/ViewModels/BaseViewModels/GenericListViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Inventory.Application.Services.Navigation;
+using MediatR;
+
+namespace Inventory.Application.ViewModels.BaseViewModels;
+
+public abstract partial class GenericListViewModel<TModel> : BaseViewModel
+    where TModel : ObservableObject
+{
+    private bool multipleSelection = false;
+
+    public GenericListViewModel(IMediator mediator, INavigationService navigationService) : base(mediator, navigationService)
+    {
+    }
+
+    public TModel? SelectedItem { get; private set; } = default;
+
+    public ObservableCollection<TModel> Items { get; private set; } = new();
+
+    public ObservableCollection<TModel> SelectedItems { get; private set; } = new();
+
+    [ICommand]
+    protected abstract void AddNew();
+
+    [ICommand]
+    protected abstract void Refresh();
+
+    [ICommand]
+    protected abstract void DeleteSelection();
+}
--- src/Inventory.Presentation/Views/DashboardViews/DashboardView.xaml.cs
+++ src/Inventory.Presentation/Views/DashboardViews/DashboardView.xaml.cs
@@ -1,14 +1,8 @@
 ﻿using Inventory.Application.ViewModels.DashboardViewModels;
 using Microsoft.UI.Xaml.Controls;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Inventory.Presentation.Views.DashboardViews;
 
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class DashboardView : Page
 {
     public DashboardView()
