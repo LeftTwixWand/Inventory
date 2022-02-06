7ed524c60b335e30a5f826b02ba25daba22dd778
Vladyslav <lefttwixwand@gmail.com>
2022-02-06 23:22:14 +0200

    feat(Presentation): Added custom CommandBar

---
+++ src/Inventory.Presentation/Controls/CustomCommandBars/CustomCommandBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Inventory.Presentation.Controls.CustomCommandBars;
+
+public sealed partial class CustomCommandBar : UserControl
+{
+    public CustomCommandBar()
+    {
+        InitializeComponent();
+    }
+}
--- src/Inventory.Presentation/Views/MainViews/MainView.xaml.cs
+++ src/Inventory.Presentation/Views/MainViews/MainView.xaml.cs
@@ -1,23 +1,18 @@
 using CommunityToolkit.Mvvm.DependencyInjection;
 using Inventory.Application.ViewModels.MainViewModels;
-using Inventory.Presentation.Views.DashboardViews;
+using Inventory.Presentation.Views.ProductsViews;
 using Microsoft.UI.Xaml.Controls;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Inventory.Presentation.Views.MainViews;
 
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class MainView : Page
 {
     public MainView()
     {
         InitializeComponent();
         MainViewModel = Ioc.Default.GetRequiredService<MainViewModel>();
-        frame.Navigate(typeof(DashboardView));
+
+        // frame.Navigate(typeof(ProductsView));
     }
 
     public MainViewModel MainViewModel { get; private set; }
