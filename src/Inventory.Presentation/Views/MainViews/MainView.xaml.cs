6e3644551e2943d60c334abf745a62a35bf622dd
Vladyslav Horbachov <0681813464.dev@gmail.com>
2022-02-11 14:19:14 +0200

    feat(Application): Added primitive navigation

--- src/Inventory.Presentation/Views/MainViews/MainView.xaml.cs
+++ src/Inventory.Presentation/Views/MainViews/MainView.xaml.cs
@@ -1,6 +1,8 @@
 using CommunityToolkit.Mvvm.DependencyInjection;
 using Inventory.Application.ViewModels.MainViewModels;
+using Inventory.Presentation.Views.DashboardViews;
 using Inventory.Presentation.Views.ProductsViews;
+using Inventory.Presentation.Views.ProductViews;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Inventory.Presentation.Views.MainViews;
@@ -12,8 +14,35 @@ public sealed partial class MainView : Page
         InitializeComponent();
         MainViewModel = Ioc.Default.GetRequiredService<MainViewModel>();
 
-        // frame.Navigate(typeof(ProductsView));
+        frame.Navigate(typeof(DashboardView));
     }
 
     public MainViewModel MainViewModel { get; private set; }
+
+    private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    {
+        switch (args.InvokedItem.ToString())
+        {
+            case "Home":
+                {
+                    frame.Navigate(typeof(DashboardView));
+                    break;
+                }
+
+            case "Products":
+                {
+                    frame.Navigate(typeof(ProductsView));
+                    break;
+                }
+
+            case "Orders":
+                {
+                    frame.Navigate(typeof(ProductView));
+                    break;
+                }
+
+            default:
+                break;
+        }
+    }
 }
