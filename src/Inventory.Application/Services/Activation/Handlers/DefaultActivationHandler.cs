6bb6649f5e45fb97af230fdd24afe6a6a41a7482
Vladislav Horbachov <50652041+LeftTwixWand@users.noreply.github.com>
2022-02-21 13:34:05 +0200

    feat(Appliection): Added ActivationService

---
+++ src/Inventory.Application/Services/Activation/Handlers/DefaultActivationHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.Application.Services.Activation.Handlers;
+using BuildingBlocks.Application.Services.DatabaseMigration;
+using HarabaSourceGenerators.Common.Attributes;
+using Microsoft.UI.Xaml;
+
+namespace AxisUNO.Shared.Services.Activation.Handlers;
+
+[Inject]
+public partial class DefaultActivationHandler : ActivationHandler<LaunchActivatedEventArgs>
+{
+    private readonly IDatabaseMigrationService _databaseMigrationService;
+
+    protected override async Task HandleInternalAsync(LaunchActivatedEventArgs? args)
+    {
+        await _databaseMigrationService.MigrateAsync();
+    }
+}
