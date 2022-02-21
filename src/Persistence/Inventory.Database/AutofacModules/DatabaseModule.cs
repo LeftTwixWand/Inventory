cfed6d42a217c74725fc653daeda83a46adfa4db
Vladislav Horbachov <50652041+LeftTwixWand@users.noreply.github.com>
2022-02-21 13:32:03 +0200

    feat(Database): Added MigrationService

--- src/Persistence/Inventory.Database/AutofacModules/DatabaseModule.cs
+++ src/Persistence/Inventory.Database/AutofacModules/DatabaseModule.cs
@@ -1,41 +1,33 @@
 ﻿using Autofac;
+using BuildingBlocks.Application.Services.DatabaseMigration;
 using BuildingBlocks.Infrastructure.Domain.UnitOfWorks.DataStorage;
 using BuildingBlocks.Infrastructure.Domain.UnitOfWorks.DomainEventsDispatching;
+using Inventory.Database.Domain.Products;
 using Inventory.Database.Domain.UnitOfWorks.DataStorage;
 using Inventory.Database.Domain.UnitOfWorks.DomainEventsDispatching;
-using Microsoft.Data.Sqlite;
+using Inventory.Database.Services.DatabaseMigration;
+using Inventory.Domain.Products;
 using Microsoft.EntityFrameworkCore;
 
 namespace Inventory.Database.AutofacModules;
 
 public sealed class DatabaseModule : Module
 {
-    private const string DatabaseName = "Inventory.db";
+    private readonly DbContextOptions<DatabaseContext> _dbContextOptions;
+
+    public DatabaseModule(DbContextOptions<DatabaseContext> dbContextOptions)
+    {
+        _dbContextOptions = dbContextOptions;
+    }
 
     protected override void Load(ContainerBuilder builder)
     {
-        builder.RegisterType<DatabaseContext>().WithParameter("options", GetOptions()).InstancePerDependency();
+        builder.RegisterType<DatabaseContext>().WithParameter("options", _dbContextOptions).InstancePerDependency();
 
         builder.RegisterType<DomainEventsProvider>().As<IDomainEventsProvider>().InstancePerLifetimeScope();
+        builder.RegisterType<DatabaseMigrationService>().As<IDatabaseMigrationService>().InstancePerLifetimeScope();
         builder.RegisterType<DataStorage>().As<IDataStorage>().InstancePerLifetimeScope();
-    }
 
-    private static DbContextOptions<DatabaseContext> GetOptions()
-    {
-        var builder = new DbContextOptionsBuilder<DatabaseContext>();
-
-        builder.UseSqlite(GetConnectionString());
-
-        return builder.Options;
-    }
-
-    private static string GetConnectionString()
-    {
-        var connectionStringBuilder = new SqliteConnectionStringBuilder()
-        {
-            DataSource = DatabaseName,
-        };
-
-        return connectionStringBuilder.ToString();
+        builder.RegisterType<ProductsRepository>().As<IProductsRepository>().InstancePerLifetimeScope();
     }
 }
---
+++ src/Persistence/Inventory.Database/Services/DatabaseMigration/DatabaseMigrationService.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.Application.Services.DatabaseMigration;
+using HarabaSourceGenerators.Common.Attributes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Database.Services.DatabaseMigration;
+
+[Inject]
+public partial class DatabaseMigrationService : IDatabaseMigrationService
+{
+    private readonly DatabaseContext _context;
+
+    public async Task MigrateAsync()
+    {
+        await _context.Database.MigrateAsync();
+    }
+}
