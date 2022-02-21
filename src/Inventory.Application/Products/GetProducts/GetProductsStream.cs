9d2cb6e348d2b562e9f0c509b74f5c23c27ed6c6
Vladislav Horbachov <50652041+LeftTwixWand@users.noreply.github.com>
2022-02-21 13:34:55 +0200

    feat(Application): Added async streams to view models

---
+++ src/Inventory.Application/Products/GetProducts/GetProductsStream.cs
@@ -0,0 +1,6 @@
+﻿using Inventory.Application.Products.Models;
+using MediatR;
+
+namespace Inventory.Application.Products.GetProducts;
+
+public record GetProductsStream : IStreamRequest<ProductModel>;
---
+++ src/Inventory.Application/Products/GetProducts/GetProductsStreamHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using HarabaSourceGenerators.Common.Attributes;
+using Inventory.Application.Products.Models;
+using Inventory.Domain.Products;
+using Mapster;
+using MediatR;
+
+namespace Inventory.Application.Products.GetProducts;
+
+[Inject]
+public partial class GetProductsStreamHandler : IStreamRequestHandler<GetProductsStream, ProductModel>
+{
+    private readonly IProductsRepository _repository;
+
+    public async IAsyncEnumerable<ProductModel> Handle(GetProductsStream request, [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        await foreach (var item in _repository.GetProductsAsync())
+        {
+            yield return item.Adapt<ProductModel>();
+        }
+    }
+}
