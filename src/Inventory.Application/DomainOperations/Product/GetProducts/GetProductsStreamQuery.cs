be08746435a7a63ce8e30a568e08e739afd463ff
Vladyslav Horbachov <50652041+LeftTwixWand@users.noreply.github.com>
2022-10-03 22:54:23 +0200

    Add first CQRS query

---
+++ src/Inventory.Application/DomainOperations/Product/GetProducts/GetProductsStreamQuery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Inventory.Domain.Products;
+using MediatR;
+
+namespace Inventory.Application.DomainOperations.Product.GetProducts;
+
+internal sealed record GetProductsStreamQuery : IStreamRequest<ProductModel>;
+
+internal sealed class GetProductsStreamQueryHandler : IStreamRequestHandler<GetProductsStream, ProductModel>
+{
+    private readonly IProductsRepository _repository;
+
+    public GetProductsStreamQueryHandler(IProductsRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async IAsyncEnumerable<ProductModel> Handle(GetProductsStream request, [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        await foreach (var item in _repository.GetProductsAsync())
+        {
+            yield return item.Adapt<ProductModel>();
+        }
+    }
+}
