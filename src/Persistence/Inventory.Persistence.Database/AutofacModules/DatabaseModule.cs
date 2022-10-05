df2f9047eeb9b441f17fbcf086249c6844bbc664
Vladyslav Horbachov <lefttwixwand@gmail.com>
2022-10-05 18:43:59 +0200

    Add Persistence.Database library

---
+++ src/Persistence/Inventory.Persistence.Database/AutofacModules/DatabaseModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Inventory.Domain.Products;
+using Inventory.Persistence.Database.Domain.Products;
+
+namespace Inventory.Persistence.Database.AutofacModules;
+
+public sealed class DatabaseModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<ProductsRepository>().As<IProductsRepository>().InstancePerLifetimeScope();
+    }
+}
---
+++ src/Persistence/Inventory.Persistence.Database/Domain/Products/ProductsRepository.cs
@@ -0,0 +1,27 @@
+﻿using Inventory.Domain.Categories;
+using Inventory.Domain.Products;
+using System.Runtime.CompilerServices;
+
+namespace Inventory.Persistence.Database.Domain.Products;
+
+internal sealed class ProductsRepository : IProductsRepository
+{
+    private readonly List<Product> _products = new(
+        new[]
+        {
+            Product.Create("Lamp1", 1, 10, Category.Create("lamps")),
+            Product.Create("Lamp2", 2, 20, Category.Create("lamps")),
+            Product.Create("Lamp3", 3, 30, Category.Create("lamps")),
+            Product.Create("Lamp4", 4, 40, Category.Create("lamps")),
+            Product.Create("Lamp5", 5, 50, Category.Create("lamps")),
+        });
+
+    public async IAsyncEnumerable<Product> GetProductsAsync([EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        foreach (var product in _products)
+        {
+            await Task.Delay(100, cancellationToken);
+            yield return product;
+        }
+    }
+}
