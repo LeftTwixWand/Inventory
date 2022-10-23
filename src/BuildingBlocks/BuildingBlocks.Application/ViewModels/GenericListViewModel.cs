09c77b3bd8b1cef27f92ff561b52b5db7f4c2fb9
Vladyslav Horbachov <50652041+LeftTwixWand@users.noreply.github.com>
2022-10-23 23:52:37 +0200

    Added GenericListViewModel

---
+++ src/BuildingBlocks/BuildingBlocks.Application/ViewModels/GenericListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using BuildingBlocks.Application.ViewModels.Messages;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace BuildingBlocks.Application.ViewModels;
+
+public abstract partial class GenericListViewModel<TModel> : ObservableRecipient, IRecipient<NewGenericItemCreatedMessage<TModel>>
+    where TModel : ObservableObject, new()
+{
+    public ObservableCollection<TModel> Items { get; } = new();
+
+    public virtual void Receive(NewGenericItemCreatedMessage<TModel> message)
+    {
+        Items.Add(message.Value);
+    }
+
+    [RelayCommand]
+    protected abstract void CreateNewItem();
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Application/ViewModels/Messages/NewGenericItemCreatedMessage.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace BuildingBlocks.Application.ViewModels.Messages;
+
+public sealed class NewGenericItemCreatedMessage<TModel> : ValueChangedMessage<TModel>
+{
+    public NewGenericItemCreatedMessage(TModel value)
+        : base(value)
+    {
+    }
+}
