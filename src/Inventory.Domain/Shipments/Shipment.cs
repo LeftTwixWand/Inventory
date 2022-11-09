f79dfe5e66ea12d03e32086dacb5c3986920a4c1
Vladyslav Horbachov <50652041+LeftTwixWand@users.noreply.github.com>
2022-11-09 23:50:41 +0100

    Refactored Order entity in the domain layer

---
+++ src/Inventory.Domain/Shipments/Shipment.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.Domain.ValueObjects;
+
+namespace Inventory.Domain.Shipments;
+
+public record Shipment : ValueObject
+{
+    public string ShipAddress { get; private set; }
+
+    public string ShipCity { get; private set; }
+
+    public string ShipRegion { get; private set; }
+
+    public string ShipCountryCode { get; private set; }
+
+    public string ShipPostalCode { get; private set; }
+
+    public DateTimeOffset? ShippedDate { get; private set; }
+
+    public DateTimeOffset? DeliveredDate { get; private set; }
+
+    public static Shipment Create()
+    {
+        return new Shipment();
+    }
+}
