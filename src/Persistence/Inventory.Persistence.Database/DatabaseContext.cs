631fcd31d295e8f3dac406f3478dc107549fec6c
Vladyslav Horbachov <vhorbachov@microsoft.com>
2022-12-12 19:25:07 +0100

    Added DBContext for SQLite

---
+++ src/Persistence/Inventory.Persistence.Database/DatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using Inventory.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Inventory.Persistence.Database;
+
+internal sealed class DatabaseContext : DbContext
+{
+    private readonly ILoggerFactory _loggerFactory;
+
+    // public DatabaseContext()
+    // {
+    //     // Required for migrations generation
+    // }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options, ILoggerFactory loggerFactory)
+        : base(options)
+    {
+        _loggerFactory = loggerFactory;
+    }
+
+    internal DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLoggerFactory(_loggerFactory);
+
+        // Required for migrations generation
+        // if (optionsBuilder.IsConfigured is false)
+        // {
+        //     var connectionStringBuilder = new SqliteConnectionStringBuilder()
+        //     {
+        //         DataSource = "Database.db",
+        //     };
+
+        // optionsBuilder.UseSqlite(connectionStringBuilder.ToString());
+        // }
+    }
+}
---
+++ src/Persistence/Inventory.Persistence.Database/Domain/Products/ProductEntityTypeConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Inventory.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inventory.Persistence.Database.Domain.Products;
+
+internal sealed class ProductEntityTypeConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(product => product.Id);
+
+        builder.Property(product => product.Id)
+            .HasColumnName(nameof(Product.Id))
+            .ValueGeneratedOnAdd();
+
+        builder.Property(product => product.Name)
+            .HasColumnName(nameof(Product.Name))
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(product => product.Category)
+            .HasColumnName(nameof(Product.Category))
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(product => product.Description)
+            .HasColumnName(nameof(Product.Description))
+            .HasMaxLength(1000);
+
+        builder.Property(product => product.Image)
+            .HasColumnName(nameof(Product.Image));
+    }
+}
