0182cc9ab133abac229d532a0f5fd03055ca5c01
Vladyslav Horbachov <vhorbachov@microsoft.com>
2023-01-12 19:00:13 +0100

    Add Warehouse tests

---
+++ tests/Inventory.Domain.UnitTests/Features/Warehouses/Create Warehouse.feature
@@ -0,0 +1,12 @@
+﻿Feature: Create Warehouse
+
+As a customer I want to manage products through the warehouse entity.
+One warehouse resposnible for one specific product.
+After warehouse been created the initial product quantity is 0.
+
+Scenario: I can create a warehouse
+	Given product id is '8c2cecbc-143f-4fd2-ac87-737190b8369e'
+	When I create a warehouse
+	Then warehouse for given product id is created
+	And products quantity is 0
+	And new WarehouseCreatedEvent is created
---
+++ tests/Inventory.Domain.UnitTests/StepDefinitions/CreateWarehouseStepDefinitions.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using Inventory.Domain.Products;
+using Inventory.Domain.Warehouses;
+using Inventory.Domain.Warehouses.Events;
+
+namespace Inventory.Domain.UnitTests.StepDefinitions;
+
+[Binding]
+public sealed class CreateWarehouseStepDefinitions
+{
+    private Guid productId;
+    private Warehouse? warehouse;
+
+    [Given(@"product id is '([^']*)'")]
+    public void GivenProductIdIs(string guidProductId)
+    {
+        productId = Guid.Parse(guidProductId);
+    }
+
+    [When(@"I create a warehouse")]
+    public void WhenICreateAWarehouse()
+    {
+        warehouse = Warehouse.Create(new ProductId(productId));
+    }
+
+    [Then(@"warehouse for given product id is created")]
+    public void ThenWarehouseForGivenProductIdIsCreated()
+    {
+        warehouse.Should().NotBeNull();
+        warehouse?.ProductId.Value.Should().Be(productId);
+    }
+
+    [Then(@"products quantity is (.*)")]
+    public void ThenProductsQuantityIs(int quantity)
+    {
+        warehouse?.Quantity.Should().Be(quantity);
+    }
+
+    [Then(@"new WarehouseCreatedEvent is created")]
+    public void ThenNewWarehouseCreatedEventIsCreated()
+    {
+        warehouse?.DomainEvents.Should().ContainSingle(e => e is WarehouseCreatedEvent);
+    }
+}
