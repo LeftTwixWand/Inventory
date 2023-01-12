a0cd94311bc3db50658adcebf648110331546e72
Vladyslav Horbachov <vhorbachov@microsoft.com>
2023-01-12 19:06:53 +0100

    Add IHostBuider to use external options

--- src/Inventory.Infrastructure/App.xaml.cs
+++ src/Inventory.Infrastructure/App.xaml.cs
@@ -5,15 +5,22 @@ using Inventory.Application.Services.Activation;
 using Inventory.Infrastructure.AutofacModules;
 using Inventory.Persistence.Database.AutofacModules;
 using Inventory.Presentation.Views.Shell;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.UI.Xaml;
 
 namespace Inventory.Infrastructure;
 
-public partial class App : Microsoft.UI.Xaml.Application
+public sealed partial class App : Microsoft.UI.Xaml.Application
 {
+    private readonly IHost _host;
+
     public App()
     {
         InitializeComponent();
+
+
+        _host = Host.CreateApplicationBuilder().Build();
 
         UnhandledException += App_UnhandledException;
 
@@ -21,24 +28,11 @@ public partial class App : Microsoft.UI.Xaml.Application
         // TODO: Update Configure method to use IHostBuilder.ConfigureAppConfiguration
         // Add Autofac.Configuration provider
         // services.Configure<LocalSettingsOptions>(hostBuilderContext.Configuration.GetSection(nameof(LocalSettingsOptions)));
-        var serviceProvider = CreateServiceProfider();
-        Ioc.Default.ConfigureServices(serviceProvider);
+        Ioc.Default.ConfigureServices(_host.Services);
     }
 
-    protected override async void OnLaunched(LaunchActivatedEventArgs args)
+    private void ConfigureServices(ContainerBuilder builder)
     {
-        base.OnLaunched(args);
-
-        var shellView = Ioc.Default.GetRequiredService<ShellView>();
-        var activationService = Ioc.Default.GetRequiredService<IActivationService>();
-
-        await activationService.ActivateAsync(shellView, args);
-    }
-
-    private static AutofacServiceProvider CreateServiceProfider()
-    {
-        var builder = new ContainerBuilder();
-
         builder
             .RegisterModule<ActivationModule>()
 
@@ -55,9 +49,28 @@ public partial class App : Microsoft.UI.Xaml.Application
             .RegisterModule<LoggingModule>()
 
             .RegisterModule<DatabaseModule>();
+    }
 
-        var container = builder.Build();
-        return new AutofacServiceProvider(container);
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        base.OnLaunched(args);
+
+        var shellView = Ioc.Default.GetRequiredService<ShellView>();
+        var activationService = Ioc.Default.GetRequiredService<IActivationService>();
+
+        await activationService.ActivateAsync(shellView, args);
+    }
+
+    private static IConfigurationRoot CreateConfigurationBuilder()
+    {
+        // Add the configuration to the ConfigurationBuilder.
+        var config = new ConfigurationBuilder();
+
+        // config.AddJsonFile comes from Microsoft.Extensions.Configuration.Json
+        // config.AddXmlFile comes from Microsoft.Extensions.Configuration.Xml
+        config.AddJsonFile("autofac.json");
+
+        return config.Build();
     }
 
     private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
