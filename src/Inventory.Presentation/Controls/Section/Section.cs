c8d0c2beb4d6c6823396ab55753b3c3d42fc3e36
Vladyslav Horbachov <0681813464.dev@gmail.com>
2023-02-15 23:57:29 +0100

    Code cleanup

--- src/Inventory.Presentation/Controls/Section/Section.cs
+++ src/Inventory.Presentation/Controls/Section/Section.cs
@@ -1,17 +1,30 @@
 ﻿using Inventory.Presentation.Common;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using System;
 
 namespace Inventory.Presentation.Controls.Section;
 
 internal sealed class Section : ContentControl
 {
+    internal static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(nameof(Header), typeof(object), typeof(Section), new PropertyMetadata(null, HeaderChanged));
+
+    internal static readonly DependencyProperty HeaderTemplateProperty = DependencyProperty.Register(nameof(HeaderTemplate), typeof(DataTemplate), typeof(Section), new PropertyMetadata(null));
+
+    internal static readonly DependencyProperty HeaderButtonGlyphProperty = DependencyProperty.Register(nameof(HeaderButtonGlyph), typeof(string), typeof(Section), new PropertyMetadata(null, HeaderButtonGlyphChanged));
+
+    internal static readonly DependencyProperty HeaderButtonLabelProperty = DependencyProperty.Register(nameof(HeaderButtonLabel), typeof(string), typeof(Section), new PropertyMetadata(null, HeaderButtonLabelChanged));
+
+    internal static readonly DependencyProperty IsButtonVisibleProperty = DependencyProperty.Register(nameof(IsButtonVisible), typeof(bool), typeof(Section), new PropertyMetadata(true, IsButtonVisibleChanged));
+
+    internal static readonly DependencyProperty FooterProperty = DependencyProperty.Register(nameof(Footer), typeof(object), typeof(Section), new PropertyMetadata(null));
+
+    internal static readonly DependencyProperty FooterTemplateProperty = DependencyProperty.Register(nameof(FooterTemplate), typeof(DataTemplate), typeof(Section), new PropertyMetadata(null));
+
     private Border? container;
     private Grid? content;
     private Button? button;
 
-    public Section()
+    internal Section()
     {
         DefaultStyleKey = typeof(Section);
     }
@@ -24,28 +37,62 @@ internal sealed class Section : ContentControl
         set => SetValue(HeaderProperty, value);
     }
 
-    public static readonly DependencyProperty HeaderProperty =
-        DependencyProperty.Register(nameof(Header), typeof(object), typeof(Section), new PropertyMetadata(null, HeaderChanged));
-
-    private static void HeaderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        var control = d as Section;
-        control?.UpdateControl();
-    }
-
     public DataTemplate HeaderTemplate
     {
         get => (DataTemplate)GetValue(HeaderTemplateProperty);
         set => SetValue(HeaderTemplateProperty, value);
     }
 
-    public static readonly DependencyProperty HeaderTemplateProperty =
-        DependencyProperty.Register(nameof(HeaderTemplate), typeof(DataTemplate), typeof(Section), new PropertyMetadata(null));
-
     public string HeaderButtonGlyph
     {
         get => (string)GetValue(HeaderButtonGlyphProperty);
         set => SetValue(HeaderButtonGlyphProperty, value);
+    }
+
+    public string HeaderButtonLabel
+    {
+        get => (string)GetValue(HeaderButtonLabelProperty);
+        set => SetValue(HeaderButtonLabelProperty, value);
+    }
+
+    public bool IsButtonVisible
+    {
+        get => (bool)GetValue(IsButtonVisibleProperty);
+        set => SetValue(IsButtonVisibleProperty, value);
+    }
+
+    public object Footer
+    {
+        get => GetValue(FooterProperty);
+        set => SetValue(FooterProperty, value);
+    }
+
+    public DataTemplate FooterTemplate
+    {
+        get => (DataTemplate)GetValue(FooterTemplateProperty);
+        set => SetValue(FooterTemplateProperty, value);
+    }
+
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        container = GetTemplateChild(nameof(container)) as Border;
+        content = GetTemplateChild(nameof(content)) as Grid;
+        button = GetTemplateChild(nameof(button)) as Button;
+
+        if (button is not null)
+        {
+            button.Click += Button_Click; ;
+        }
+
+        IsEnabledChanged += Section_IsEnabledChanged; ;
+    }
+
+    private static void HeaderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = d as Section;
+        control?.UpdateControl();
     }
 
     private static void HeaderButtonGlyphChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -54,26 +101,10 @@ internal sealed class Section : ContentControl
         control?.UpdateControl();
     }
 
-    public static readonly DependencyProperty HeaderButtonGlyphProperty = DependencyProperty.Register(nameof(HeaderButtonGlyph), typeof(string), typeof(Section), new PropertyMetadata(null, HeaderButtonGlyphChanged));
-
-    public string HeaderButtonLabel
-    {
-        get => (string)GetValue(HeaderButtonLabelProperty);
-        set => SetValue(HeaderButtonLabelProperty, value);
-    }
-
     private static void HeaderButtonLabelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         var control = d as Section;
         control?.UpdateControl();
-    }
-
-    public static readonly DependencyProperty HeaderButtonLabelProperty = DependencyProperty.Register(nameof(HeaderButtonLabel), typeof(string), typeof(Section), new PropertyMetadata(null, HeaderButtonLabelChanged));
-
-    public bool IsButtonVisible
-    {
-        get => (bool)GetValue(IsButtonVisibleProperty);
-        set => SetValue(IsButtonVisibleProperty, value);
     }
 
     private static void IsButtonVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -81,26 +112,6 @@ internal sealed class Section : ContentControl
         var control = d as Section;
         control?.UpdateControl();
     }
-
-    public static readonly DependencyProperty IsButtonVisibleProperty = DependencyProperty.Register(nameof(IsButtonVisible), typeof(bool), typeof(Section), new PropertyMetadata(true, IsButtonVisibleChanged));
-
-    public object Footer
-    {
-        get => GetValue(FooterProperty);
-        set => SetValue(FooterProperty, value);
-    }
-
-    public static readonly DependencyProperty FooterProperty = DependencyProperty.Register(nameof(Footer), typeof(object), typeof(Section), new PropertyMetadata(null));
-
-    public DataTemplate FooterTemplate
-    {
-        get => (DataTemplate)GetValue(FooterTemplateProperty);
-        set => SetValue(FooterTemplateProperty, value);
-    }
-    public bool OnIsEnabledChanged { get; private set; }
-
-    public static readonly DependencyProperty FooterTemplateProperty = DependencyProperty.Register(nameof(FooterTemplate), typeof(DataTemplate), typeof(Section), new PropertyMetadata(null));
-
 
     private void UpdateControl()
     {
@@ -120,7 +131,7 @@ internal sealed class Section : ContentControl
         UpdateContainer();
     }
 
-    public void UpdateContainer()
+    private void UpdateContainer()
     {
         if (content is null)
         {
@@ -139,30 +150,13 @@ internal sealed class Section : ContentControl
         }
     }
 
-    protected override void OnApplyTemplate()
+    private void Button_Click(object sender, RoutedEventArgs e)
     {
-        base.OnApplyTemplate();
-
-        container = GetTemplateChild(nameof(container)) as Border;
-        content = GetTemplateChild(nameof(content)) as Grid;
-        button = GetTemplateChild(nameof(button)) as Button;
-
-        if (button is not null)
-        {
-            button.Click += OnClick;
-        }
-
-        IsEnabledChanged += OnIsEnabledChanged; ;
+        HeaderButtonClick?.Invoke(this, e);
     }
 
-    private void OnIsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+    private void Section_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
     {
-        throw new NotImplementedException();
+        UpdateContainer();
     }
-
-    private void OnClick(object sender, RoutedEventArgs e)
-    {
-        HeaderButtonClick ?.Invoke(this, e);
-    }
-
 }
