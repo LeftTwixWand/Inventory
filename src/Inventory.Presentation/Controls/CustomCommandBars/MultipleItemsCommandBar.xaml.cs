677c3a4ef6cfeaf0b96afe2f447508f7b6d8a769
Vladyslav Horbachov <vhorbachov@microsoft.com>
2023-03-10 18:37:08 +0100

    Separate CustomCommandBar into two controls for single and multiple items management

---
+++ src/Inventory.Presentation/Controls/CustomCommandBars/MultipleItemsCommandBar.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Inventory.Presentation.Controls.CustomCommandBars;
+
+internal sealed partial class MultipleItemsCommandBar : UserControl
+{
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(nameof(Title), typeof(string), typeof(MultipleItemsCommandBar), new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty IsSelectionModeProperty =
+        DependencyProperty.Register(nameof(IsSelectionMode), typeof(bool), typeof(MultipleItemsCommandBar), new PropertyMetadata(false));
+
+    public static readonly DependencyProperty AddCommandProperty =
+        DependencyProperty.Register(nameof(AddCommand), typeof(ICommand), typeof(MultipleItemsCommandBar), new PropertyMetadata(null));
+
+    public static readonly DependencyProperty DeleteCommandProperty =
+        DependencyProperty.Register(nameof(DeleteCommand), typeof(ICommand), typeof(MultipleItemsCommandBar), new PropertyMetadata(null));
+
+    public MultipleItemsCommandBar()
+    {
+        InitializeComponent();
+    }
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public bool IsSelectionMode
+    {
+        get => (bool)GetValue(IsSelectionModeProperty);
+        set => SetValue(IsSelectionModeProperty, value);
+    }
+
+    public ICommand AddCommand
+    {
+        get => (ICommand)GetValue(AddCommandProperty);
+        set => SetValue(AddCommandProperty, value);
+    }
+
+    public ICommand DeleteCommand
+    {
+        get => (ICommand)GetValue(DeleteCommandProperty);
+        set => SetValue(DeleteCommandProperty, value);
+    }
+}
---
+++ src/Inventory.Presentation/Controls/CustomCommandBars/SingleItemCommandBar.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Inventory.Presentation.Controls.CustomCommandBars;
+
+internal sealed partial class SingleItemCommandBar : UserControl
+{
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(nameof(Title), typeof(string), typeof(SingleItemCommandBar), new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty IsEditModeProperty =
+        DependencyProperty.Register(nameof(IsEditMode), typeof(bool), typeof(SingleItemCommandBar), new PropertyMetadata(false));
+
+    public static readonly DependencyProperty EditCommandProperty =
+        DependencyProperty.Register(nameof(EditCommand), typeof(ICommand), typeof(SingleItemCommandBar), new PropertyMetadata(null));
+
+    public static readonly DependencyProperty CancelCommandProperty =
+        DependencyProperty.Register(nameof(CancelCommand), typeof(ICommand), typeof(SingleItemCommandBar), new PropertyMetadata(null));
+
+    public static readonly DependencyProperty SaveCommandProperty =
+        DependencyProperty.Register(nameof(SaveCommand), typeof(ICommand), typeof(SingleItemCommandBar), new PropertyMetadata(null));
+
+    public static readonly DependencyProperty DeleteCommandProperty =
+        DependencyProperty.Register(nameof(DeleteCommand), typeof(ICommand), typeof(SingleItemCommandBar), new PropertyMetadata(null));
+
+    public SingleItemCommandBar()
+    {
+        InitializeComponent();
+    }
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public bool IsEditMode
+    {
+        get => (bool)GetValue(IsEditModeProperty);
+        set => SetValue(IsEditModeProperty, value);
+    }
+
+    public ICommand EditCommand
+    {
+        get => (ICommand)GetValue(EditCommandProperty);
+        set => SetValue(EditCommandProperty, value);
+    }
+
+    public ICommand CancelCommand
+    {
+        get => (ICommand)GetValue(CancelCommandProperty);
+        set => SetValue(CancelCommandProperty, value);
+    }
+
+    public ICommand SaveCommand
+    {
+        get => (ICommand)GetValue(SaveCommandProperty);
+        set => SetValue(SaveCommandProperty, value);
+    }
+
+    public ICommand DeleteCommand
+    {
+        get => (ICommand)GetValue(DeleteCommandProperty);
+        set => SetValue(DeleteCommandProperty, value);
+    }
+}
